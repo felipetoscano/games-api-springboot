d9dc544e56cd3646846fe38750aba7af9db6a710
Andre1875 <61830793+Andre1875@users.noreply.github.com>
2020-04-28 23:28:05 -0300

    Add files via upload
    Classe model com validações

--- games/src/main/java/br/com/fiap/model/GameModel.java
+++ games/src/main/java/br/com/fiap/model/GameModel.java
@@ -1,5 +1,8 @@
 package br.com.fiap.model;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Size;
+
 public class GameModel {
 	
 	private long id;
@@ -23,27 +26,63 @@ public class GameModel {
 	public long getId() {
 		return id;
 	}
+	
 	public void setId(long id) {
 		this.id = id;
 	}
+	
+	@Size(min = 2, max = 256, message = "Url deve ter no mínimo 2 e no máximo 256 caracteres")
 	public String getImage() {
 		return image;
 	}
+	
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	@Size(min = 2, max = 40, message = "Nome deve ter no mínimo 2 e no máximo 40 caracteres")
 	public String getName() {
 		return name;
 	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Size(min = 2, max = 40, message = "Categoria deve ter no mínimo 2 e no máximo 40 caracteres")
 	public String getCategory() {
 		return category;
 	}
+	
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	@DecimalMin(value = "0.1", message = "O preço deve ter um valor minimo de 0.0")
 	public float getPrice() {
 		return price;
 	}
+	
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	@Size(min = 2, max = 256, message = "Descrição deve ter no mínimo 2 e no máximo 256 caracteres")
 	public String getDescription() {
 		return description;
 	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Size(min = 2, max = 256, message = "Url deve ter no mínimo 2 e no máximo 256 caracteres")
 	public String getUrlBuy() {
 		return urlBuy;
 	}
 	
+	public void setUrlBuy(String urlBuy) {
+		this.urlBuy = urlBuy;
+	}
 	
 }
